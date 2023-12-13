0ed8ae3b36c7ce544a08df6758b29d46a0d0f370
Barış Adsız <123736772+BarisAdsiz@users.noreply.github.com>
2023-12-13 14:18:51 +0300

    Add files via upload

---
+++ src/ders_3/Ders_3.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ders_3;
+
+/**
+ *
+ * @author Barış Adsız
+ */
+public class Ders_3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int sayi = 12;
+        System.out.println("Sayi=" + sayi);
+        
+        int sayi2 = 24;
+        int toplam = sayi + sayi2;
+        
+        System.out.println("Toplam=" + toplam);
+        
+        
+        
+        String yazi = "Merhaba Ben";
+        String yazi2 = "Baris Adsiz";
+        System.out.println(yazi + " " + yazi2);
+    }
+    
+}
